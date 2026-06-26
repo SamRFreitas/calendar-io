f82e66a1813feb43758b7ae1c6996a302ab1d807
SamuelDac <samuel@dacxi.com>
2026-06-26 12:48:14 -0300

    refactor: setup directory to add E2D tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
