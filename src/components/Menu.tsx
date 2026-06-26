c43bc8f438ae116529c2f027d6b22d0d8602dab9
SamuelDac <samuel@dacxi.com>
2026-06-26 15:54:00 -0300

    style: change background color

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,7 +7,7 @@ interface MenuProps {
 export default function Menu({ children }: MenuProps) {
 
   return (
-    <div className="flex justify-between gap-4 p-4 bg-[#1a1a1a] rounded-xl shadow-sm mb-6">
+    <div className="flex justify-between gap-4 p-4 bg-primary rounded-xl shadow-sm mb-6">
       {children}
     </div>
   )
