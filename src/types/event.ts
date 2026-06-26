85d087d702b46c883b91de83ebbc63b93f0356d7
SamuelDac <samuel@dacxi.com>
2026-06-26 15:54:44 -0300

    improvement: remove slot

--- src/types/event.ts
+++ src/types/event.ts
@@ -1,6 +1,6 @@
 export interface Event {
     id: string
-    type: 'meeting' | 'slot' | 'task'
+    type: 'meeting' | 'task'
     name: string
     startDate: string // ISO: "2026-06-22T09:00:00"
     endDate: string
