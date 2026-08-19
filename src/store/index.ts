5ee8c7c2f43ecc5058eaa8fddcf0c48fc3b4e84e
SamRFreitas <samuelfreitassi@gmail.com>
2026-08-19 15:48:40 -0300

    feat: Added ui reducer to store's reducer object; imports and integrates uiReducer from uiSlice.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import eventsReducer from './eventsSlice'
+import uiReducer from './uiSlice'
 
 export const store = configureStore({
     reducer: {
         events: eventsReducer,
+        ui: uiReducer,
     },
 })
 
